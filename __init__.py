72c10dd7950943237ef350a9a33f0884941f0019
chenxiliang <chenxi.liang2012@gmail.com>
2017-10-12 14:01:04 -0500

    break up website into separate html files

--- __init__.py
+++ __init__.py
@@ -1,14 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    return render_template("main.html")
-
-@app.route('/dashboard/')
 def dashboard():
     return render_template("dashboard.html")
+
+#@app.route('/dashboard/')
+#def dashboard():
+    #return render_template("dashboard.html")
+
+@app.route('/dashboardodd/')
+def dashboardodd():
+    return render_template("dashboardodd.html")
+
+@app.route('/loadFiles', methods=['GET','POST'])
+def loadFiles():
+	#if request.method == 'POST':
+	#	return redirect(url_for('dashboard'))
+	return render_template("loadFiles.html")
+
+@app.route('/TransformData', methods=['GET','POST'])
+def TransformData():
+	return render_template("TransformData.html")
+
+@app.route('/FFT_Preview', methods=['GET','POST'])
+def FFT_Preview():
+	return render_template("FFT_Preview.html")
+
+@app.route('/Configure', methods=['GET','POST'])
+def Configure():
+	return render_template("Configure.html")
+
+@app.route('/Result', methods=['GET','POST'])
+def Result():
+	return render_template("Result.html")
+
+
+
 
 if __name__ == "__main__":
     app.run()
